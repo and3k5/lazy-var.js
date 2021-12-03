67f470b81751dad3a30fbdf89f4b9599704b2dac
Anders K <and3k5@users.noreply.github.com>
2021-12-03 13:23:30 +0100

    Add project

---
+++ spec/test-spec.js
@@ -0,0 +1,70 @@
+const { lazy } = require("../lib/lazy-var");
+
+describe("lazy-var", function () {
+
+    it("can lazy load", function () {
+        var value = lazy(() => 20);
+        var outputValue = value.get();
+        expect(outputValue).toBe(20);
+    })
+
+    it("can lazy load when async", async function () {
+        var value = lazy(async () => 20);
+        var outputValue = await value.get();
+        expect(outputValue).toBe(20);
+    })
+
+    it("does not call twice", function () {
+        var i = 0;
+        var value = lazy(() => i++);
+        var outputValue1 = value.get();
+        var outputValue2 = value.get();
+        expect(outputValue1).toBe(0);
+        expect(outputValue2).toBe(0);
+    })
+
+    it("does not call twice when async", async function () {
+        var i = 0;
+        var value = lazy(async () => i++);
+        var outputValue1 = await value.get();
+        var outputValue2 = await value.get();
+        expect(outputValue1).toBe(0);
+        expect(outputValue2).toBe(0);
+    })
+
+    const wait = async function (n) {
+        return new Promise((res, rej) => {
+            setTimeout(() => res(), n)
+        });
+    }
+
+    it("can handle multiple calls", function () {
+        var i = 0;
+        var value = lazy(() => i++);
+
+        var asyncFunc = async function () {
+            var v = value.get();
+            await wait(50);
+            return v;
+        };
+        var p1 = asyncFunc();
+        var p2 = asyncFunc();
+
+        p1.then((v) => expect(v).toBe(0));
+        p2.then((v) => expect(v).toBe(0));
+    })
+
+    it("can handle multiple calls when async", async function () {
+        var i = 0;
+        var value = lazy(async () => {
+            await wait(50);
+            return i++
+        });
+        var outputValue1 = value.get();
+        var outputValue2 = value.get();
+        await wait(50);
+        var values = await Promise.all([outputValue1, outputValue2]);
+        expect(values[0]).toBe(0);
+        expect(values[1]).toBe(0);
+    })
+})
