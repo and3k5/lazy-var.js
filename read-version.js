75d46bb5a00185a248485552b7e578db2fb50eb3
Anders K <and3k5@users.noreply.github.com>
2023-01-16 13:11:19 +0100

    Fix error on version bump

--- read-version.js
+++ read-version.js
@@ -1,5 +1,5 @@
 var version = "v"+require("./package.json").version;
-const command = ("git tag -a "+version+" -m \"Tag version "+version+"\"");
+const command = ("git tag -f -a "+version+" -m \"Tag version "+version+"\"");
 if (process.argv.findIndex(x => x.toLowerCase() === "--run-command")) {
     console.log("Running command: "+command);
     require("child_process").execSync(command);
